e5f914c5053a65357579fb3e7dce5c5b0a530601
Azeem Subhani <azeemforall4446303@gmail.com>
2023-11-23 18:45:31 +0500

    Initial Setup Drizzle ORM

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,26 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as dotenv from "dotenv";
+import * as schema from "../../../migration/schema";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+
+dotenv.config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) {
+  console.log("❌ - Cannot Find Database URL.");
+}
+
+const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
+const db = drizzle(client, schema);
+const migrateDatabase = async () => {
+  try {
+    console.log("🟢 - Migrating Database");
+    await migrate(db, { migrationsFolder: "migrations" });
+    console.log("🎉 - Migrated...");
+  } catch (err) {
+    console.log("❌ - Error Migrating Database.");
+    console.log(err);
+  }
+};
+
+export default db;
